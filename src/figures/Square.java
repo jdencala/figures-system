408db391ef1395fcec0010130530c4cfc3f962d4
jdencala <jdencalahuaya@gmail.com>
2024-11-28 12:24:32 -0500

    Add(I): Add Square Drawing and Calculation methods

--- src/figures/Square.java
+++ src/figures/Square.java
@@ -8,6 +8,20 @@ public class Square {
         this.length = length;
     }
 
+    //method that draws a Square
+    public void drawSquare() {
+        System.out.println("****************************************");
+        System.out.println("Drawing a Square with length: " + length + "\n");
+        System.out.println("========================================");
+        System.out.println("Perimeter: " + calculatePerimeter());
+        System.out.println("========================================");
+        //Print Area
+    }
+
+    public int calculatePerimeter() {
+        return length * 4;
+    }
+
     public int getLength() {
         return length;
     }
